97d612d5e09e432cfb6dd206d527fd88892811eb
Greyson-Thomas <greyson.thomas35@gmail.com>
2022-03-22 18:21:48 -0500

    [+] Member Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,32 @@ Member.create!([{
     uin: "000000001",
     telephone: "(972)888-8888",
     email: "ePerkins@tamu.edu"
+},
+{
+    std_first_name: "Ana",
+    std_last_name: "Huff",
+    uin: "000000002",
+    telephone: "(979)999-9999",
+    email: "aHuff@tamu.edu" 
+},
+{
+    std_first_name: "Ruben",
+    std_last_name: "Dean",
+    uin: "000000003",
+    telephone: "(979)777-7777",
+    email: "rDean@tamu.edu"
+},
+{
+    std_first_name: "Doyle",
+    std_last_name: "Weaver",
+    uin: "000000004",
+    telephone: "(214)666-6666",
+    email: "dWeaver@tamu.edu"
+},
+{
+    std_first_name: "Della",
+    std_last_name: "Kim",
+    uin: "000000005",
+    telephone: "(572)555-5555",
+    email: "dKim@tamu.edu"
 }])

cea395edf8b839613d8aa6ea136c59e054f9a9ad
Dallin Keesling <dkeeslin@tamu.edu>
2022-03-28 22:06:52 -0500

    Horse CRUD

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -2,4 +2,43 @@ class HorsesController < ApplicationController
   def index
     @horses = Horse.all
   end
+
+  def edit
+    @horse = Horse.find(params[:id])
+  end
+
+  def show
+    id = params[:id]
+    @horse = Horse.find(id)
+  end
+
+  def new
+    @horse = Horse.new
+  end
+
+  def create 
+    horse = Horse.create(horse_params)
+    redirect_to horse_path(horse)
+  end
+
+  def update
+    @horse = Horse.find(params[:id])
+
+    @horse.update(horse_params)
+
+    redirect_to horse_path(@horse)
+  end
+
+  def destroy
+    @horse = Horse.find(params[:id])
+    @horse.destroy
+
+    redirect_to horses_path
+  end
+
+  private
+
+  def horse_params
+    params.require(:horse).permit(:horse_name,:brand_number,:availability)
+  end
 end

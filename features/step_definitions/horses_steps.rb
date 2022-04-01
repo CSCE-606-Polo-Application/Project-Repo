ee65a4b7827a8c8154a79da7d222136b9d4740a8
Dallin Keesling <dkeeslin@tamu.edu>
2022-03-31 22:50:34 -0500

    Added testing for adding a horse

--- features/step_definitions/horses_steps.rb
+++ features/step_definitions/horses_steps.rb
@@ -1,15 +1,33 @@
-Given('I am on the herd page') do
+  Given('I am on the herd page') do
     visit horses_path
   end
   
   Given('I am an officer') do
     
   end
+
+  Given("I am on the New Horse page") do
+    visit new_horse_path
+  end
   
   When('I click on {string}') do |string|
-    click_button string
+    click_on string
   end
   
   Then('I will be on the New Horse page') do
-    pending # Write code here that turns the phrase above into concrete actions
+    expect(page.current_url).to eq(page.current_host+new_horse_path)
+  end
+
+  Given('{string} is set to {string}') do |string, string2|
+    begin
+      fill_in string, with: string2
+
+    rescue
+      select string2, from: "horse_"+string.downcase
+    end
+
+  end
+
+  Then('I will see {string}') do |string|
+    expect(page).to have_text string
   end

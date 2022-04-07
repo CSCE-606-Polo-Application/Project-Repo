3f87f2038d8e5abec48af90f2042c3744a816613
Dallin Keesling <dkeeslin@tamu.edu>
2022-04-06 21:25:54 -0500

    One more dependency for member destruction. Not sure why it worked before this change and then stopped again.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ApplicationRecord
-    has_many :riding_times
+    has_many :riding_times, dependent: :destroy
     has_many :riding_preferences, dependent: :destroy
     has_many :horses, :through => :riding_preferences
     has_one :officer, dependent: :destroy

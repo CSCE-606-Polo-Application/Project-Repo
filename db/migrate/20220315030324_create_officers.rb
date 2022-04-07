0fcb497e970bb28348a0cd8954ce3067fae5d798
Dallin Keesling <dkeeslin@tamu.edu>
2022-04-06 20:36:52 -0500

    Cleaning up some stuff that was missed during the merge with main

--- db/migrate/20220315030324_create_officers.rb
+++ db/migrate/20220315030324_create_officers.rb
@@ -1,10 +1,7 @@
 class CreateOfficers < ActiveRecord::Migration[7.0]
   def change
     create_table :officers do |t|
-      t.string :name
       t.string :position
-      t.string :telephone
-      t.string :email
 
       t.timestamps
     end

608fdf7e14d55f40b52369364167a0ed00969e02
vaidyular@gmail.com <vaidyular@gmail.com>
2022-04-11 01:24:23 -0500

    Contact Us Page with the ability to send emails. Overdue but here it is

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'riding_times/index'
   get 'officers/index'
   get 'members/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :horses, :officers, :riding_times
 
@@ -16,4 +17,6 @@ end
 
 Rails.application.routes.draw do
   resources :members
+  resources :contacts, only: [:new, :create]
 end
+

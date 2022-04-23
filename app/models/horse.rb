0ed41a3fcc92c36747f5fc8108fe2800f9b551cd
Greyson-Thomas <greyson.thomas35@gmail.com>
2022-04-22 20:41:04 -0500

    model validation

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -2,4 +2,9 @@ class Horse < ApplicationRecord
     has_many :riding_times
     has_many :riding_preferences, dependent: :destroy
     has_many :members, :through => :riding_preferences
+
+    validates :horse_name , presence: true 
+    validates :brand_number , presence: true , numericality: { only_integer: true }
+    validates :availability , presence: true
+    
 end

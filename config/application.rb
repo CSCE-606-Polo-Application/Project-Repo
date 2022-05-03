8ea983b4a49de4eba30a46e9656bc161672eefd6
Dallin Keesling <dkeeslin@tamu.edu>
2022-05-02 22:34:26 -0500

    changing config.autoloader

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module ProjectRepoUmlbased
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    config.autoloader = :classic
     config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end

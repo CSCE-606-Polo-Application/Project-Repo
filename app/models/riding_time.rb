2513b55e3326b9bdf5460ef125397724dae64d79
tifosiBarca <g.owenhe@gmail.com>
2022-05-03 12:17:07 -0500

    small fix

--- app/models/riding_time.rb
+++ app/models/riding_time.rb
@@ -1,5 +1,5 @@
 class RidingTime < ApplicationRecord
-    belongs_to :member
-    belongs_to :horse
-    belongs_to :riding_schedule
+    belongs_to :member optional: true
+    belongs_to :horse optional: true
+    belongs_to :riding_schedule optional: true
 end

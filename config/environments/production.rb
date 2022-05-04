de47bebfd61d71d46c804c46beef663a8b0c595e
Dallin Keesling <dkeeslin@tamu.edu>
2022-05-04 15:54:23 -0500

    added comment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.
+  #testing
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
